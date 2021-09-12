633de98ca00f3bf5c551db84697c754026468d92
Yishay Garame <54310381+YishayGarame@users.noreply.github.com>
2021-09-12 17:22:33 +0300

    first

---
+++ client-server/client.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import socket
+
+HOST = '10.0.5.4'  # The server's hostname or IP address
+PORT = 8081        # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b'Hello, world')
+    data = s.recv(1024)
+
+print('Received', repr(data))
+
+
+
+
+
+
